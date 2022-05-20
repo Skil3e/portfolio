1512bc9e61dc78ab595690a674bb576ab9410547
Skil3e <manosm.design@gmail.com>
2022-05-20 12:45:56 +0300

    fix: focus first focusable element on smooth scroll

--- src/components/button/btnUtils.ts
+++ src/components/button/btnUtils.ts
@@ -1,13 +1,13 @@
 export function smoothScroll( id: string, offset: number = 0 ) {
-    const element = document.getElementById( id );
-    if (!element) {
-        return
-    }
-    history.replaceState( {}, "", `#${ id }` )
-    const top = window.scrollY + element.getBoundingClientRect().top - offset;
-    window.scrollTo( {
+    const element = document?.getElementById( id );
+    if (!element) return;
+    history?.replaceState( {}, "", `#${ id }` )
+    const top = window?.scrollY + element.getBoundingClientRect().top - offset;
+    window?.scrollTo( {
         behavior: 'smooth',
         top
     } );
-    element.focus()
+    const focusableItems = [ 'a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', '[tabindex="0"]' ]; //a list of items that should be focusable.
+    const focusable = element.querySelectorAll( focusableItems.join( ", " ) )?.[0] as HTMLElement | null
+    focusable?.focus()
 }
