5db6cbdac3a79c42ba27bd396343175239de91ae
Skil3e <manosm.design@gmail.com>
2022-06-03 12:28:29 +0300

    perf: added recaptcha to Script component

--- src/app/WrapPageElement.tsx
+++ src/app/WrapPageElement.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AnimatePresence } from "framer-motion";
-import { GatsbyBrowser } from "gatsby";
+import { Script, GatsbyBrowser } from "gatsby";
 
 const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ( { element, props } ) => {
     // const locale = props.pageContext.locale as TypeLocale
     return (
         <>
+            { process.env.GATSBY_GOOGLE_RECAPTCHA_SITE_KEY &&
+                <Script key={ "recaptcha-script" } src={ `https://www.google.com/recaptcha/api.js?render=${ process.env.GATSBY_GOOGLE_RECAPTCHA_SITE_KEY }` }/>
+            }
             <AnimatePresence exitBeforeEnter>
                 <React.Fragment key={ props.location.pathname }>
                     { element }
--- src/app/onRenderBody.tsx
+++ src/app/onRenderBody.tsx
@@ -2,29 +2,24 @@ import React from "react";
 import { GatsbySSR } from "gatsby";
 
 const onRenderBody: GatsbySSR["onRenderBody"] = ( { setHeadComponents } ) => {
-    const googleReCaptchaSiteKey = process.env.GATSBY_GOOGLE_RECAPTCHA_SITE_KEY
-    if (googleReCaptchaSiteKey) {
-        const src = `https://www.google.com/recaptcha/api.js?render=${ googleReCaptchaSiteKey }`;
-        setHeadComponents( [
-            <script key={ "recaptcha-script" } src={ src }/>,
-            <link
-                key="agency"
-                rel="preload"
-                href="/fonts/AgencyFB-Bold.ttf"
-                as="font"
-                type="font/ttf"
-                crossOrigin="anonymous"
-            />,
-            <link
-                key="manrope"
-                rel="preload"
-                href="/fonts/Manrope-VariableFont_wght.ttf"
-                as="font"
-                type="font/ttf"
-                crossOrigin="anonymous"
-            />
-        ] )
-    }
+    setHeadComponents( [
+        <link
+            key="agency"
+            rel="preload"
+            href="/fonts/AgencyFB-Bold.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+        />,
+        <link
+            key="manrope"
+            rel="preload"
+            href="/fonts/Manrope-VariableFont_wght.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+        />
+    ] )
 }
 
 export default onRenderBody
